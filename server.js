31230dbe6399fcf9df9d7c7a1c046bfdf996e4a4
Gustav Lindqvist <gustav@glindqvist.se>
2014-02-07 23:23:48 +0100

    finished response with image upload

--- server.js
+++ server.js
@@ -7,16 +7,11 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-        
-        var content = route(handle, pathname);
-        
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(content);
-        response.end();
+        route(handle, pathname, response, request);
     }
-    
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
 }
